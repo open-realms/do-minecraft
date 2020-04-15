85f52807059e0aa2e2449db349c695f3bd7ea01f
Andrew Woodruff <44043065+andrewgwoodruff@users.noreply.github.com>
2020-04-15 16:38:00 -0600

    Restart and command endpoints (#16)
    * Done with restart endpoint. Command endpoint does not seem to be working.
    
    * Add JSON parsing to fix command endpoint.

--- testserver/minecraft.js
+++ testserver/minecraft.js
@@ -12,6 +12,7 @@ let serverStatus = MINECRAFT_STATUS.NOT_RUNNING;
 
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get('/', (req, res) => res.send('MinecraftExpressApp is up and running!'));
@@ -29,11 +30,32 @@ app.post('/start', (req, res) => {
   res.send({ status: MINECRAFT_STATUS.RUNNING });
 });
 
+app.post('/restart', (req, res) => {
+  restartMinecraft();
+  res.send({ status: MINECRAFT_STATUS.RUNNING });
+});
+
 app.delete('/shutdown', (req, res) => {
   if (serverStatus == MINECRAFT_STATUS.RUNNING) {
     shutdownMinecraft();
   }
   res.send({ status: MINECRAFT_STATUS.NOT_RUNNING });
+});
+
+app.post('/command', (req, res) => {
+  const command = req.body;
+  minecraftServerProcess.stdin.write(command.command + '\n');
+
+  let buffer = [];
+  let collector = data => {
+    data = data.toString();
+    buffer.push(data.split(']: ')[1]);
+  };
+  minecraftServerProcess.stdout.on('data', collector);
+  setTimeout(function() {
+    minecraftServerProcess.stdout.removeListener('data', collector);
+    res.send(buffer.join(''));
+  }, 250);
 });
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'));
@@ -62,6 +84,15 @@ function startMinecraft() {
 function shutdownMinecraft() {
   minecraftServerProcess.kill();
   serverStatus = MINECRAFT_STATUS.NOT_RUNNING;
+}
+
+function restartMinecraft() {
+  if (serverStatus == MINECRAFT_STATUS.RUNNING) {
+    shutdownMinecraft();
+    startMinecraft();
+  } else {
+    startMinecraft();
+  }
 }
 
 function getMinecraftStatus() {
