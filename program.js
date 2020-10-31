0d6de40f6842db6c18c65fdb3ede468898d821ae
Andrew Woodruff <agwoodruff93@gmail.com>
2020-10-31 11:06:01 -0600

    add several methods

--- program.js
+++ program.js
@@ -4,7 +4,7 @@ const { MinecraftService } = require('./dist');
 const service = new MinecraftService();
 
 // hardcode the correct id here when testing the various methods below
-const id = 212567519;
+const id = 214710939;
 
 // phony id to use to test error handling of the various methods below
 const idOfDropletThatDoesNotExist = 2;
@@ -22,6 +22,12 @@ const options = {
 //   .then(droplet => {
 //     console.log(JSON.stringify(droplet));
 //   })
+//   .catch(err => {
+//     console.log(JSON.stringify(err));
+//   });
+
+// service
+//   .startMinecraftRemotely(id)
 //   .catch(err => {
 //     console.log(JSON.stringify(err));
 //   });
