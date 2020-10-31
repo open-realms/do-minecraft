b8bb5a7acaa8840402bba840826dd34940d0c762
Andrew Woodruff <agwoodruff93@gmail.com>
2020-10-31 11:11:47 -0600

    add new methods to program.js for testing

--- program.js
+++ program.js
@@ -6,6 +6,9 @@ const service = new MinecraftService();
 // hardcode the correct id here when testing the various methods below
 // const id = 214710939;
 
+// hardcode whatever command here when testing the command endpoint
+// const command = 'sample command';
+
 // phony id to use to test error handling of the various methods below
 const idOfDropletThatDoesNotExist = 2;
 
@@ -26,12 +29,6 @@ const options = {
 //     console.log(JSON.stringify(err));
 //   });
 
-// service
-//   .startMinecraftRemotely(id)
-//   .catch(err => {
-//     console.log(JSON.stringify(err));
-//   });
-
 // service.stopMinecraftDroplet(id).catch(err => {
 //   console.log(err);
 // });
@@ -40,6 +37,36 @@ const options = {
 //   console.log(err);
 // });
 
-service.killMinecraftDroplet(id).catch(err => {
-  console.log(JSON.stringify(err));
-});
+// service.killMinecraftDroplet(id).catch(err => {
+//   console.log(JSON.stringify(err));
+// });
+
+// service
+//   .startMinecraftRemotely(id)
+//   .catch(err => {
+//     console.log(JSON.stringify(err));
+//   });
+
+// service
+//   .stopMinecraftRemotely(id)
+//   .catch(err => {
+//     console.log(JSON.stringify(err));
+//   });
+
+// service
+//   .restartMinecraftRemotely(id)
+//   .catch(err => {
+//     console.log(JSON.stringify(err));
+//   });
+
+// service
+//   .getMinecraftServerStatus(id)
+//   .catch(err => {
+//     console.log(JSON.stringify(err));
+//   });
+
+// service
+//   .sendMinecraftCommand(id, command)
+//   .catch(err => {
+//     console.log(JSON.stringify(err));
+//   });
