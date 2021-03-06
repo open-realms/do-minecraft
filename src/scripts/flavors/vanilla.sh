02ef439d1cd8a901e373e85f538e73c31893b7c1
John Woodruff <johnwoodruff91@gmail.com>
2021-03-06 09:54:07 -0700

    Try using curl instead of wget

--- src/scripts/flavors/vanilla.sh
+++ src/scripts/flavors/vanilla.sh
@@ -13,6 +13,6 @@ stdout.write(str(load(urlopen(url))['downloads']['server']['url']))
 ")
 
 rm -rf /home/mcuser/express-minecraft/minecraft/minecraft_server-run.jar
-wget -O /home/mcuser/express-minecraft/minecraft/minecraft_server-run.jar "$URL"
+curl -f $URL -o /home/mcuser/express-minecraft/minecraft/minecraft_server-run.jar
 echo "eula=true" > /home/mcuser/express-minecraft/minecraft/eula.txt
 echo "enable-query=true" > /home/mcuser/express-minecraft/minecraft/server.properties
